50c597136b5361b3daf1c4415a7014512cedc473
fernandosagastume <fernando.sagastume@galileo.edu>
2021-09-23 23:34:18 -0600

    Se hacen cambios en el administrador de negocios.

--- pages/negocios/registro.vue
+++ pages/negocios/registro.vue
@@ -377,7 +377,7 @@ import autHeader from '../../src/services/auth-header';
 import authHeader from '../../src/services/auth-header';
 
 export default {
-  
+
 
   mounted() {
     //this.geolocate()
@@ -528,10 +528,11 @@ export default {
           usuarioId: JSON.parse(sessionStorage.getItem('usuario')).id
         }
 
-        await this.$api.post("/negocio", params, { headers : autHeader()}).then( data => {
+        await this.$api.post("/negocio", params).then( data => {
           this.$alert.registro_exitoso()
           this.$router.push({ path: '/' })
         } ).catch(data => {
+          console.error(data)
           this.$alert.error('Ocurrió un error en el registro, vuelve a intentarlo', 'Error Interno')
         })
 
